8ce06358d945409a09de965766b5aca518b455f6
netui <netuiorg@gmail.com>
2022-05-01 20:33:48 +0200

    Material picker

--- Net.UI.Sandbox/Net.UI.Sandbox/MainPage.xaml.cs
+++ Net.UI.Sandbox/Net.UI.Sandbox/MainPage.xaml.cs
@@ -2,17 +2,28 @@
 
 public partial class MainPage : ContentPage
 {
+    public List<string> Items { get; set; } = new List<string>
+    {
+        "Red",
+        "Black"
+    };
+
 	public MainPage()
 	{
 		InitializeComponent();
         button.Pressed += Button_Pressed;
         button.Released += Button_Released;
         button.Clicked += Button_Clicked;
+
+        mp.BindingContext = this;
 	}
 
     private void Button_Clicked(object sender, EventArgs e)
     {
         label.Text += "\nClicked";
+        Items = new List<string> { "A", "B", "C", "D" };
+        OnPropertyChanged(nameof(Items));
+
     }
 
     private void Button_Released(object sender, EventArgs e)
