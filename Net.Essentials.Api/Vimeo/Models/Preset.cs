bf03efa15ba364dfc572bb5a5ef591e1c98ae6e3
netui <netuiorg@gmail.com>
2023-09-06 10:30:49 +0300

    [Vimeo] Presets

---
+++ Net.Essentials.Api/Vimeo/Models/Preset.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Net.Essentials.Vimeo.Models
+{
+    public class Preset
+    {
+        public object Metadata { get; set; }
+        public string Name { get; set; }
+        public EmbedSettings Settings { get; set; }
+        public string Uri { get; set; }
+        public User User { get; set; }
+    }
+}
