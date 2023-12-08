4abd3ab0f0695ff2d56af334337095d15a7358da
netui <netuiorg@gmail.com>
2023-12-08 16:15:50 +0100

    1.0.18: Primitive

---
+++ Net.Essentials/Components/Primitive.cs
@@ -0,0 +1,16 @@
+﻿namespace Net.Essentials
+{
+    public class Primitive<T>
+    {
+        public Primitive() { }
+        public Primitive(T value)
+        {
+            Value = value;
+        }
+
+        public T Value { get; set; }
+
+        public static implicit operator T(Primitive<T> p) => p == default ? default : p.Value;
+        public static implicit operator Primitive<T>(T v) => new Primitive<T>(v);
+    }
+}
