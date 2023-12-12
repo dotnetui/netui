93608c620ffdd92172c161f3fad8e3f78efe57cc
netui <netuiorg@gmail.com>
2023-12-12 18:50:37 +0200

    1.0.21 Async CommandModel

--- Net.Essentials/Components/CommandModel.cs
+++ Net.Essentials/Components/CommandModel.cs
@@ -1,32 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Input;
 
-namespace Net.Essentials.Components
+namespace Net.Essentials
 {
     public class CommandModel<T> : ICommand
     {
+        public Func<Task<T>> ExecuteTask { get; set; }
         public Action<T> ExecuteAction { get; set; }
         public Func<T, bool> CanExecuteFunc { get; set; }
 
         public event EventHandler CanExecuteChanged;
+        public bool WaitUntilDone { get; set; }
 
-        public CommandModel(Action<T> execute, Func<T, bool> canExecute = null)
+        volatile bool isExecuting = false;
+
+        public CommandModel(Action<T> execute, Func<T, bool> canExecute = null, bool waitUntilDone = true)
         {
             ExecuteAction = execute;
             CanExecuteFunc = canExecute;
+            WaitUntilDone = waitUntilDone;
+        }
+
+        public CommandModel(Func<Task<T>> execute, Func<T, bool> canExecute = null, bool waitUntilDone = true)
+        {
+            ExecuteTask = execute;
+            CanExecuteFunc = canExecute;
+            WaitUntilDone = waitUntilDone;
         }
 
 
         public bool CanExecute(object parameter)
         {
-            return CanExecuteFunc == null || CanExecuteFunc((T)parameter);
+            return !isExecuting && (CanExecuteFunc == null || CanExecuteFunc((T)parameter));
         }
 
         public void Execute(object parameter)
         {
-            ExecuteAction?.Invoke((T)parameter);
+            if (isExecuting && WaitUntilDone) return;
+
+            isExecuting = WaitUntilDone;
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+            if (ExecuteTask != null) Task.Run(async () =>
+            {
+                try
+                {
+                    await ExecuteTask();
+                }
+                catch { throw; }
+                finally
+                {
+                    isExecuting = false;
+                    CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+                }
+
+            });
+            else
+            {
+                ExecuteAction?.Invoke((T)parameter);
+                isExecuting = false;
+                CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+            }
         }
     }
 
