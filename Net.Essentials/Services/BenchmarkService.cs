b2c764ba0d34bf368bd93d270ae2a19b17c95cd7
netui <netuiorg@gmail.com>
2024-01-06 19:10:01 +0200

    1.0.25 Override LogFunc for BenchmarkService

--- Net.Essentials/Services/BenchmarkService.cs
+++ Net.Essentials/Services/BenchmarkService.cs
@@ -15,10 +15,19 @@ namespace Net.Essentials
         public bool ShouldPrintAfterStops = true;
         public bool Echo = true;
         public uint AutoPrintInterval = 2000;
+        public Action<object> LogFunc;
 
         private readonly ConcurrentDictionary<Benchmark, string> messages = new ConcurrentDictionary<Benchmark, string>();
         private readonly ConcurrentBag<(DateTime Date, TimeSpan Duration, string Message)> finishedTasks = new ConcurrentBag<(DateTime Date, TimeSpan Duration, string Message)>();
         private volatile bool isPrintTimerActive = false;
+
+        void WriteLine(object s)
+        {
+            if (LogFunc != null)
+                LogFunc(s);
+            else
+                Console.WriteLine(s);
+        }
 
         public IDisposable StartBenchmark([CallerMemberName] string message = null)
         {
@@ -30,7 +39,7 @@ namespace Net.Essentials
             benchmark.Stopped += Benchmark_Stopped;
             if (Echo)
             {
-                Console.WriteLine($"[Benchmark Start | {messages.Count} active] {message}");
+                WriteLine($"[Benchmark Start | {messages.Count} active] {message}");
                 StartPrintTimer();
             }
             return benchmark;
@@ -53,7 +62,7 @@ namespace Net.Essentials
                 finishedTasks.Add((DateTime.Now, benchmark.Elapsed, $"{startMessage} {message}"));
                 if (Echo)
                 {
-                    Console.WriteLine($"[Benchmark End | {messages.Count} active] Elapsed: {benchmark.StopToString()} {startMessage} {message}");
+                    WriteLine($"[Benchmark End | {messages.Count} active] Elapsed: {benchmark.StopToString()} {startMessage} {message}");
                     if (ShouldPrintAfterStops)
                         PrintStatus();
                 }
@@ -82,17 +91,17 @@ namespace Net.Essentials
             if (!IsEnabled)
                 return;
 
-            Console.WriteLine(" ");
-            Console.WriteLine("------------------------------------");
-            Console.WriteLine("ACTIVE BENCHMARKS");
+            WriteLine(" ");
+            WriteLine("------------------------------------");
+            WriteLine("ACTIVE BENCHMARKS");
             foreach (var message in messages)
-                Console.WriteLine($"{message.Key}\t{message.Value}");
-            Console.WriteLine("------------------------------------");
-            Console.WriteLine("RECENT BENCHMARKS");
+                WriteLine($"{message.Key}\t{message.Value}");
+            WriteLine("------------------------------------");
+            WriteLine("RECENT BENCHMARKS");
             foreach (var message in finishedTasks.OrderByDescending(x => x.Date).Take(5))
-                Console.WriteLine(message);
-            Console.WriteLine("------------------------------------");
-            Console.WriteLine("FINISHED BENCHMARKS");
+                WriteLine(message);
+            WriteLine("------------------------------------");
+            WriteLine("FINISHED BENCHMARKS");
             var summaries = finishedTasks.GroupBy(x => x.Message).Select(x => new
             {
                 Duration = TimeSpan.FromSeconds(x.Sum(y => y.Duration.TotalSeconds)),
@@ -100,8 +109,8 @@ namespace Net.Essentials
                 Count = x.Count()
             }).OrderByDescending(x => x.Duration);
             foreach (var item in summaries)
-                Console.WriteLine($"{item.Duration}\tx{item.Count,-5}\t\t{item.Duration.TotalSeconds / item.Count:N3} s/t\t\t{item.Message}");
-            Console.WriteLine(" ");
+                WriteLine($"{item.Duration}\tx{item.Count,-5}\t\t{item.Duration.TotalSeconds / item.Count:N3} s/t\t\t{item.Message}");
+            WriteLine(" ");
         }
 
         public void Throw(Exception ex, [CallerMemberName] string caller = null)
@@ -122,7 +131,7 @@ namespace Net.Essentials
 
         public virtual void Report(Exception ex, [CallerMemberName] string caller = null)
         {
-            Console.WriteLine($"[Exception Thrown by {caller}] {ex}");
+            WriteLine($"[Exception Thrown by {caller}] {ex}");
         }
     }
 }
